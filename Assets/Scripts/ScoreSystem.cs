8aea4aae710b54c9119600dce4f89139cd171e79
Jacky Yang <jackyyang267@gmail.com>
2021-02-06 12:26:35 -0500

    Added score system

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,13 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
+    [SerializeField] float score;
+    [SerializeField] float scoreOnHit;
+
+    [Header("Object References")]
+    [SerializeField] PlayerBehaviour player;
+
+    public Action OnScoreChanged;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        player.OnBulletFired += AddScoreOnHit;
+    }
+
+    private void OnDisable()
+    {
+        player.OnBulletFired -= AddScoreOnHit;
+    }
+
+
+    private void AddScoreOnHit(bool miss, Vector2 position)
+    {
+        OnScoreChanged?.Invoke();
     }
 
     // Update is called once per frame
